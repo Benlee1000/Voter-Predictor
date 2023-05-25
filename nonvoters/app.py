3f48e466624acc0b99c2032d2cb16b29fcf75f35
Benlee1000 <benny01lee@gmail.com>
2023-05-24 18:09:22 -0700

    Added basic dropdown input functionality

--- nonvoters/app.py
+++ nonvoters/app.py
@@ -11,6 +11,15 @@ def show_predict_form():
     predicted = nonvoters.find_predictions(model)
     image_buffer = nonvoters.save_model()
     accuracy, variance, mse, precision, recall = nonvoters.find_stats()
-    return render_template('resultsform.html', predicted=predicted, buffer_encode=image_buffer, best_model=model_params, accuracy=accuracy, variance=variance, mse=mse, precision=precision, recall=recall)
-    
+    return render_template('predictorform.html', predicted=predicted, buffer_encode=image_buffer, best_model=model_params, accuracy=accuracy, variance=variance, mse=mse, precision=precision, recall=recall)
+
+@app.route('/results', methods=['POST'])
+def show_result_form():
+    if request.method == 'POST':
+        education = request.form['education']
+        race = request.form['race']
+        gender = request.form['gender']
+        income = request.form['income']
+        return render_template('resultsform.html', model_output=education+race+gender+income)
+
 app.run("localhost", "9999", debug=True) 
